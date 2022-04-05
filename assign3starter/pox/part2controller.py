5bfab8abdebdb969b7a3fad2ef6f60e063ad423c
JoeySo22 <jesoto4@miners.utep.edu>
2022-04-05 16:41:53 -0600

    more changes

--- assign3starter/pox/part2controller.py
+++ assign3starter/pox/part2controller.py
@@ -17,10 +17,9 @@ class Firewall (object):
     connection.addListeners(self)
 
     #add switch rules here
+    self.macToPort = {} # Keep track of which host is at what port
 
     # Create an action that takes any ipv4 src & dest, and icmp protocol, and accept
-    flow_mod_1 = of.ofp_flow_mod()
-    flow_mod_1.match = of.ofp_match(
     # Create an action that takes any kind of src & dest, and arp protocol, and accept
     # Create an action that takes an ipv4 src & dest, and any protocol, and drop
 
@@ -31,6 +30,27 @@ class Firewall (object):
     """
 
     packet = event.parsed # This is the parsed packet data.
+    
+    # Add packet to the mactoport
+    self.macToPort[packet.src] = event.port
+
+    # Define accept
+    def accept(priority = 5000):
+        msg = of.ofp_flow_mod()
+        msg.priority = priority
+        msg.match = of.ofp_match.from_packet(packet, event.port)
+        msg.actions.append(of.ofp_action_output(port = self.macToPort[packet.dst]))
+        msg.data = event.ofp
+        self.connection.send(msg)
+
+    # Define drop
+    def drop():
+        msg = of.ofp_flow_mod()
+        msg.match = of.ofp_match.from_packet(packet)
+        msg.buffer_id = event.ofp.buffer_id
+        msg.in_port = event.port
+        self.connection.send(msg)
+
     if not packet.parsed:
       log.warning("Ignoring incomplete packet")
       return
@@ -39,9 +59,19 @@ class Firewall (object):
     ipv4_packet = packet.find('ipv4')
     if ipv4_packet:
         # 1) ipv4 src | ipv4 dest | icmp | accept
-        tcp_packet = event.parsed.find('icmp')
-    
+        icmp_packet = event.parsed.find('icmp')
+        if icmp_packet:
+            accept(priority = 4000) #Highest priority
+        else:
+            # 3) ipv4 src | ipv4 dest | any protocol | drop
+            drop()
+    else:
+        arp_packet = event.parsed.find('arp')
+        # 2) any src | any dest | arp | accept
+        if arp_packet:
+            accept(priority = 3000) # second priority
 
+    
     packet_in = event.ofp # The actual ofp_packet_in message.
     print ("Unhandled packet :" + str(packet.dump()))
 
