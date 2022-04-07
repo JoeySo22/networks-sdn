b914220e708ad4526b4dabcdd203a84a9168babd
JoeySo22 <jesoto4@miners.utep.edu>
2022-04-06 18:35:51 -0600

    Working part 2

--- assign3starter/pox/part2controller.py
+++ assign3starter/pox/part2controller.py
@@ -21,18 +21,22 @@ class Firewall (object):
     # Create an action that takes any ipv4 src & dest, and icmp protocol, and accept
     first_rule = of.ofp_flow_mod()
     first_rule.priority = 3000
-    first_rule.match.dl_type = 0x0806 # Value for ARP packets
+    first_rule.match.dl_type = 0x0800 # Value for ipv4
+    first_rule.match.nw_proto = 1 # icmp selection
     first_rule.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD)) # Send to all
     self.connection.send(first_rule)
     # Create an action that takes any kind of src & dest, and arp protocol, and accept
     second_rule = of.ofp_flow_mod()
     second_rule.priority = 2000
-    second_rule.match.dl_type = 0x0800 # ip here
+    second_rule.match.dl_type = 0x0806 # arp here
     second_rule.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD)) 
-    second_rule.nw_proto = 1
     self.connection.send(second_rule)
     # Create an action that takes an ipv4 src & dest, and any protocol, and drop
-    # It seems that by exclusivity we will drop the other protocols
+    third_rule = of.ofp_flow_mod()
+    third_rule.priority = 1000
+    third_rule.match.dl_type = 0x0800
+    third_rule.buffer_id = None
+    self.connection.send(third_rule)
 
   def _handle_PacketIn (self, event):
     """
